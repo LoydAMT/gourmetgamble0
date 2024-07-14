608cdf70abcc6c18f9adad3e2074d8f6e9c82353
M4monski <162318730+M4monski@users.noreply.github.com>
2024-07-14 16:11:57 +0000

    SLgiht adjustment nalang kapoy na

--- src/App.js
+++ src/App.js
@@ -144,9 +144,6 @@ function AppContent({
           Contact Us
         </button>
         <button className="footer-item" onClick={() => setShowPrivacyModal(true)}>
-          Feedback
-        </button>
-        <button className="footer-item" onClick={() => setShowPrivacyModal(true)}>
           Privacy Policy
         </button>
       </footer>

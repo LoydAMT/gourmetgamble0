e520421b23341acfb2f5b610cbceeae7466c10a8
arariadne <elaishaarias@gmail.com>
2024-07-24 20:58:11 +0800

    Update ContactUsModal.js

--- src/ContactUsModal.js
+++ src/ContactUsModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './ContactUsModal.css'; // Import the CSS file
+import './ContactUsModal.css'; 
 
 const ContactUsModal = ({ showModal, setShowModal }) => {
     const closeModal = () => {
